f667343c1b4f961022f2cbf03e833ab4c5b607e9
Agustin Sau <sau.agustin.i@gmail.com>
2023-09-09 10:09:08 -0300

    dao

--- interfaces/DAO.java
+++ interfaces/DAO.java
@@ -1,5 +1,10 @@
 package integrador1Arqui.interfaces;
 
 public interface DAO {
+	public int insert();
+	 public boolean deleteById(int id);
+	 public boolean modify(int id);
+	 //public collection selectAll();
+	 
 
 }
